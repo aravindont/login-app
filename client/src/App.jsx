fdb4a1a84a0056d16f93e9b727c56395a3f26e19
Aravind104 <ontagodiaravind@gmail.com>
2023-01-10 17:45:01 +0530

    add routes using react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,47 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Username from "./components/Username";
+import Reset from "./components/Reset";
+import Register from "./components/Register";
+import Recovery from "./components/Recovery";
+import Profile from "./components/Profile";
+import Password from "./components/Password";
+import PageNotFound from "./components/PageNotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Username />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/password",
+    element: <Password />,
+  },
+  {
+    path: "/recovery",
+    element: <Recovery />,
+  },
+  {
+    path: "/reset",
+    element: <Reset />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    path: "*",
+    element: <PageNotFound />,
+  },
+]);
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
+  );
 }
